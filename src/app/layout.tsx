0838c88982f5eb2f287a8a3122a33a14676f79df
Rohan <az9589317@gmail.com>
2025-11-03 05:50:38 +0000

    Select verification method:
    AdSense code snippet
    
    Ads.txt snippet
    
    Met

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,19 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6434494026178547"
+          crossOrigin="anonymous"
+        ></script>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
           rel="stylesheet"
